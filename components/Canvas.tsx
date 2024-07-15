61d79c78991d0e2e155428dd77fd8b52cc985cb6
mahdixmohammad <mahdi3mohammad@gmail.com>
2024-07-15 03:17:46 -0400

    Add panning functionality, resize all candlesticks by a factor of 0.5, implement horizontal and vertical crosshairs, remove redundant code

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -15,7 +15,7 @@ export default function Canvas() {
 	}
 
 	// initializes state of application
-	const [openingPrice, setOpeningPrice] = useState(200);
+	const [openingPrice, setOpeningPrice] = useState(300);
 	const [data, setData] = useState<Record<string, Candlestick>>({});
 	const [count, setCount] = useState(0);
 	const [fastForwarding, setForwarding] = useState(false);
@@ -23,8 +23,13 @@ export default function Canvas() {
 
 	// state of canvas
 	const [scale, setScale] = useState(1);
+	const [isPanning, setIsPanning] = useState(false);
+	const [panOffset, setPanOffset] = useState({ x: 0, y: 0 });
+	const [lastPanPosition, setLastPanPosition] = useState({ x: 0, y: 0 });
 
 	// set React DOM references
+	const horizontalLineRef = useRef<HTMLDivElement | null>(null);
+	const verticalLineRef = useRef<HTMLDivElement | null>(null);
 	const canvasRef = useRef<HTMLCanvasElement | null>(null);
 	const contextRef = useRef<CanvasRenderingContext2D | null>(null);
 
@@ -40,53 +45,54 @@ export default function Canvas() {
 
 	const drawCandlestick = useCallback((candlestick: Candlestick, index: number) => {
 		const c = contextRef.current!;
-		const x: number = index * 40;
+		const x: number = index * 20;
 
 		if (candlestick.color === "green") {
 			// creates the candlestick body
-			c.fillStyle = candlestick.color;
-			c.fillRect(x, candlestick.openingPrice, 40, -candlestick.bodyHeight);
+			c.fillStyle = "rgb(0, 167, 114)";
+			c.fillRect(x, candlestick.openingPrice, 20, -candlestick.bodyHeight);
 
 			// creates the candlestick wicks
-			c.strokeStyle = candlestick.color;
-			c.lineWidth = 4;
+			c.strokeStyle = "rgb(0, 167, 114)";
+			c.lineWidth = 2;
 			c.beginPath();
 			// moves the line in between the candlestick body at its top
-			c.moveTo(x + 20, candlestick.closingPrice);
+			c.moveTo(x + 10, candlestick.closingPrice);
 			// draws the top wick
-			c.lineTo(x + 20, candlestick.closingPrice - candlestick.topWickHeight);
+			c.lineTo(x + 10, candlestick.closingPrice - candlestick.topWickHeight);
 			// moves the line in between the candlestick body at its bottom
-			c.moveTo(x + 20, candlestick.openingPrice);
+			c.moveTo(x + 10, candlestick.openingPrice);
 			// draws the bottom wick
-			c.lineTo(x + 20, candlestick.openingPrice + candlestick.bottomWickHeight);
+			c.lineTo(x + 10, candlestick.openingPrice + candlestick.bottomWickHeight);
 			c.stroke();
 		} else {
 			// creates the candlestick body
-			c.fillStyle = candlestick.color;
-			c.fillRect(x, candlestick.openingPrice, 40, candlestick.bodyHeight);
+			c.fillStyle = "rgb(255, 50, 50)";
+			c.fillRect(x, candlestick.openingPrice, 20, candlestick.bodyHeight);
 
-			c.strokeStyle = candlestick.color;
-			c.lineWidth = 4;
+			c.strokeStyle = "rgb(255, 50, 50)";
+			c.lineWidth = 2;
 			// creates the candlestick wicks
 			c.beginPath();
 			// moves the line in between the candlestick body at its top
-			c.moveTo(x + 20, candlestick.openingPrice);
+			c.moveTo(x + 10, candlestick.openingPrice);
 			// draws the top wick
-			c.lineTo(x + 20, candlestick.openingPrice - candlestick.topWickHeight);
+			c.lineTo(x + 10, candlestick.openingPrice - candlestick.topWickHeight);
 			// moves the line in between the candlestick body at its bottom
-			c.moveTo(x + 20, candlestick.closingPrice);
+			c.moveTo(x + 10, candlestick.closingPrice);
 			// draws the bottom wick
-			c.lineTo(x + 20, candlestick.closingPrice + candlestick.bottomWickHeight);
+			c.lineTo(x + 10, candlestick.closingPrice + candlestick.bottomWickHeight);
 			c.stroke();
 		}
 	}, []);
 
 	const redrawCanvas = useCallback(
-		(newScale: number) => {
+		(newScale: number, panOffset: { x: number; y: number }) => {
 			const c = contextRef.current!;
 			c.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height);
 			c.save();
 			c.scale(newScale, newScale);
+			c.translate(panOffset.x, panOffset.y);
 			Object.keys(data).forEach(key => {
 				const candlestick = data[key];
 				drawCandlestick(candlestick, Number(key) - 1);
@@ -97,24 +103,18 @@ export default function Canvas() {
 	);
 
 	const handleAdd = useCallback(() => {
-		// map numbers to a color and its rgb value
-		const colors = new Map([
-			[0, ["red", "rgb(255, 50, 50)"]],
-			[1, ["green", "rgb(0, 167, 114)"]],
-		]);
-		// randomly picks either 0 or 1
+		// randomly picks either 0 or 1 (0 -> red, 1 -> green)
 		const random = getRandomInt(0, 1);
 		// destructures the array mapped to 0 or 1
-		const [color, rgb] = colors.get(random)!;
+		const color = random === 0 ? "red" : "green";
 
 		// randomly pick the sizes of the candlestick body and wicks
-		let bodyHeight = getRandomInt(12.5, 100);
-		const topWickHeight = getRandomInt(0, 45);
-		const bottomWickHeight = getRandomInt(0, 45);
-		let closingPrice: number;
+		const bodyHeight = getRandomInt(25, 200);
+		const topWickHeight = getRandomInt(0, 90);
+		const bottomWickHeight = getRandomInt(0, 90);
 
 		// green means negative y (candle goes up), red means positive y (candle goes down)
-		closingPrice = color === "green" ? openingPrice - bodyHeight : openingPrice + bodyHeight;
+		let closingPrice: number = color === "green" ? openingPrice - bodyHeight : openingPrice + bodyHeight;
 
 		// creates new entry for data
 		let newCandlestick: Candlestick = {
@@ -139,8 +139,8 @@ export default function Canvas() {
 	const handleDelete = useCallback(() => {
 		if (count > 0) {
 			const c = contextRef.current!;
-			c.clearRect((count - 1) * 40, 0, 40, canvasRef.current!.height);
-			const newPrice = count > 1 ? data[count - 1]["closingPrice"] : 200;
+			c.clearRect((count - 1) * 20, 0, 20, canvasRef.current!.height);
+			const newPrice = count > 1 ? data[count - 1]["closingPrice"] : 300;
 			setOpeningPrice(newPrice);
 			delete data[count];
 			setData(data);
@@ -152,26 +152,9 @@ export default function Canvas() {
 	useEffect(() => {
 		// uses setTimeout to ensure that variables load first
 		setTimeout(() => {
-			redrawCanvas(scale);
+			redrawCanvas(scale, panOffset);
 		});
 	});
-
-	// runs anytime the state of fastForwarding / fasatBackwarding changes
-	useEffect(() => {
-		let addTimer: any;
-		let deleteTimer: any;
-
-		if (fastForwarding) {
-			addTimer = setInterval(handleAdd, 500);
-		} else if (fastBackwarding) {
-			deleteTimer = setInterval(handleDelete, 500);
-		}
-
-		return () => {
-			clearInterval(addTimer); // Cleanup: clear the interval when component unmounts
-			clearInterval(deleteTimer); // Cleanup: clear the interval when component unmounts
-		};
-	}, [fastForwarding, fastBackwarding, handleAdd, handleDelete]);
 
 	// initial render runs this function
 	useEffect(() => {
@@ -181,23 +164,123 @@ export default function Canvas() {
 		canvas.height = canvas.offsetHeight;
 		const c = canvas.getContext("2d")!;
 		contextRef.current = c;
+	}, []);
+
+	// handles all functionality related to zooming and panning
+	useEffect(() => {
+		const canvas: HTMLCanvasElement = canvasRef.current!;
+		const horizontalLine: HTMLDivElement = horizontalLineRef.current!;
+		const verticalLine: HTMLDivElement = verticalLineRef.current!;
 
 		const handleZoom = (event: WheelEvent) => {
 			event.preventDefault();
+
 			const scaleFactor = event.deltaY > 0 ? 0.9 : 1.1;
-			setScale(prevScale => prevScale * scaleFactor);
+			setScale(prevScale => {
+				const newScale = prevScale * scaleFactor;
+				// Define your minimum and maximum scale values
+				const minScale = 0.1;
+				const maxScale = 3;
+
+				// Clamp the new scale value within the min and max values
+				return Math.min(Math.max(newScale, minScale), maxScale);
+			});
 		};
 
-		canvas.addEventListener("wheel", handleZoom);
+		const handlePanStart = (event: MouseEvent) => {
+			canvas.style.cursor = "grabbing";
+			setIsPanning(true);
+			setLastPanPosition({ x: event.clientX, y: event.clientY });
+		};
 
+		const handlePanMove = (event: MouseEvent) => {
+			if (!isPanning) return;
+			const deltaX = event.clientX - lastPanPosition.x;
+			const deltaY = event.clientY - lastPanPosition.y;
+
+			setPanOffset(prevOffset => ({
+				x: prevOffset.x + deltaX / scale,
+				y: prevOffset.y + deltaY / scale,
+			}));
+			setLastPanPosition({ x: event.clientX, y: event.clientY });
+		};
+
+		const handlePanEnd = () => {
+			canvas.style.cursor = "crosshair";
+			setIsPanning(false);
+		};
+
+		// positions crosshairs to where the cursor moves
+		const handleLineMove = (e: any) => {
+			const xPosition = e.pageX;
+			const yPosition = e.pageY;
+			horizontalLine.style.left = `${canvas.getBoundingClientRect().left}px`;
+			horizontalLine.style.top = `${yPosition}px`;
+			verticalLine.style.left = `${xPosition}px`;
+			horizontalLine.style.display = "block";
+			verticalLine.style.display = "block";
+		};
+
+		// makes crosshairs invisible when users are not on the canvas
+		const handleLineLeave = () => {
+			horizontalLine.style.display = "none";
+			verticalLine.style.display = "none";
+		};
+
+		// zoom and panning functionality
+		canvas.addEventListener("wheel", handleZoom);
+		canvas.addEventListener("mousedown", handlePanStart);
+		canvas.addEventListener("mousemove", handlePanMove);
+		canvas.addEventListener("mouseup", handlePanEnd);
+		canvas.addEventListener("mouseleave", handlePanEnd);
+
+		// crosshair functionality
+		canvas.addEventListener("mousemove", handleLineMove);
+		canvas.addEventListener("mouseleave", handleLineLeave);
+
+		// cleanup
 		return () => {
 			canvas.removeEventListener("wheel", handleZoom);
+			canvas.removeEventListener("mousedown", handlePanStart);
+			canvas.removeEventListener("mousemove", handlePanMove);
+			canvas.removeEventListener("mouseup", handlePanEnd);
+			canvas.removeEventListener("mouseleave", handlePanEnd);
+
+			canvas.removeEventListener("mousemove", handleLineMove);
+			canvas.removeEventListener("mouseleave", handleLineLeave);
 		};
-	}, []);
+	}, [scale, isPanning, panOffset, lastPanPosition]);
+
+	// runs anytime the state of fastForwarding / fasatBackwarding changes
+	useEffect(() => {
+		let addTimer: any;
+		let deleteTimer: any;
+
+		if (fastForwarding) {
+			addTimer = setInterval(handleAdd, 250);
+		} else if (fastBackwarding) {
+			deleteTimer = setInterval(handleDelete, 250);
+		}
+
+		return () => {
+			clearInterval(addTimer); // Cleanup: clear the interval when component unmounts
+			clearInterval(deleteTimer); // Cleanup: clear the interval when component unmounts
+		};
+	}, [fastForwarding, fastBackwarding, handleAdd, handleDelete]);
 
 	return (
 		<>
-			<canvas ref={canvasRef} className="w-11/12 h-[600px] bg-gray-900 mx-auto border-2 border-white"></canvas>
+			<div
+				ref={horizontalLineRef}
+				className="z-40 absolute w-11/12 left-[46px] border-t-2 border-dotted border-white pointer-events-none"
+				style={{ display: "none" }}
+			></div>
+			<div
+				ref={verticalLineRef}
+				className="z-50 absolute h-[600px] top-[132px] border-l-2 border-dotted border-white pointer-events-none"
+				style={{ display: "none" }}
+			></div>
+			<canvas ref={canvasRef} className="w-11/12 h-[600px] bg-gray-900 mx-auto border-2 border-white cursor-crosshair"></canvas>
 			<div className="flex justify-center items-center gap-4 md:gap-6 mx-auto bg-gray-900 w-10/12 sm:w-2/4 md:w-[450px] h-20 my-8 rounded-2xl border-2 border-white">
 				<Image
 					className="w-10 md:w-[50px] mx-4 md:mx-8 cursor-pointer"
